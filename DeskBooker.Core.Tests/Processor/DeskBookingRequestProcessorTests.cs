155f1f0d882dd4cc250c993d071a25330e6bc29b
Jakub Przybyłowski <jakub.przybylowski95@gmail.com>
2020-09-09 14:38:10 +0200

    Dodaj pliki projektów.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,48 @@
+﻿using DeskBooker.Core.Domain;
+using System;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        private readonly DeskBookingRequestProcessor _processor;
+
+        public DeskBookingRequestProcessorTests()
+        {
+            _processor = new DeskBookingRequestProcessor();
+        }
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            // Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Jakub",
+                LastName = "Przybyłowski",
+                Email = "jakub.przybylowski95@gmail.com",
+                Date = new DateTime(2020, 1, 28)
+            };
+
+
+            // Act
+            DeskBookingResult result = _processor.BookDesk(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+
+            var exeption = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+
+            Assert.Equal("request", exeption.ParamName);
+        }
+    }
+}
